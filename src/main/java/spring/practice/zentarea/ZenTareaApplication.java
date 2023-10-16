2e12f083d02a108cdfbfe8d5ccffb11e13a341f2
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-10-16 21:27:25 +0530

    [init] Project initialised

---
+++ src/main/java/spring/practice/zentarea/ZenTareaApplication.java
@@ -0,0 +1,13 @@
+package spring.practice.zentarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenTareaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenTareaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/practice/zentarea/ZenTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.practice.zentarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenTareaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
